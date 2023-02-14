5b469cf329345f6f91bbd8b6aef913f9db6d7ae2
Cauê <cauegastaldi@outlook.com>
2023-02-14 18:50:50 -0300

    Tudo quase pronto, faltando o toast.

--- app/src/main/java/br/edu/ifsp/scl/ads/cadastro/Formulario.java
+++ app/src/main/java/br/edu/ifsp/scl/ads/cadastro/Formulario.java
@@ -1,4 +1,78 @@
 package br.edu.ifsp.scl.ads.cadastro;
 
+import androidx.annotation.NonNull;
+
 public class Formulario {
+
+    private String nomeCompleto;
+    private String telefone;
+    private String email;
+    private boolean listaEmails;
+    private String sexo;
+    private String cidade;
+    private String uf;
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isListaEmails() {
+        return listaEmails;
+    }
+
+    public void setListaEmails(boolean listaEmails) {
+        this.listaEmails = listaEmails;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "Nome completo: " + nomeCompleto + ", telefone: " + telefone + ", email: " + email
+                + "Incluso na lista de emails: " + listaEmails + ", sexo: " + sexo + ", cidade: " +
+                cidade + ", UF: " + uf;
+    }
 }
--- app/src/main/java/br/edu/ifsp/scl/ads/cadastro/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/ads/cadastro/MainActivity.java
@@ -3,12 +3,58 @@ package br.edu.ifsp.scl.ads.cadastro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Formulario form;
+    private Button limparBt;
+    private Button salvarBt;
+    private EditText nomeEt;
+    private EditText telefoneEt;
+    private EditText emailEt;
+    private CheckBox listaEmailCb;
+    private RadioButton masculinoRb;
+    private RadioButton femininoRb;
+    private EditText cidade;
+    private Spinner uf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nomeEt = findViewById(R.id.nomeEt);
+        telefoneEt = findViewById(R.id.telefoneEt);
+        emailEt = findViewById(R.id.emailEt);
+        listaEmailCb = findViewById(R.id.listaEmailCb);
+        masculinoRb = findViewById(R.id.masculinoRb);
+        femininoRb = findViewById(R.id.femininoRb);
+        cidade = findViewById(R.id.cidadeEt);
+        uf = findViewById(R.id.ufSp);
+
+        form = new Formulario();
+        salvarBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                form = new Formulario();
+                form.setNomeCompleto(nomeEt.getText().toString());
+                form.setTelefone(telefoneEt.getText().toString());
+                form.setEmail(emailEt.getText().toString());
+                form.setListaEmails(listaEmailCb.isChecked());
+                String sexo = (masculinoRb.isChecked()) ?
+                        masculinoRb.getText().toString() : femininoRb.getText().toString();
+                form.setSexo(sexo);
+                form.setCidade(cidade.getText().toString());
+                form.setUf(uf.getSelectedItem().toString());
+            }
+        });
+
     }
 }
